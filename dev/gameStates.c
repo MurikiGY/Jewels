e74406f3bdf5f6cd33af994b26e10f8606e2916e
Muriki <mgy20@inf.ufpr.br>
2023-02-03 08:45:00 -0300

    Added some resources

---
+++ dev/gameStates.c
@@ -0,0 +1,23 @@
+#include "gameStates.h"
+
+
+void load_game(){
+
+
+}
+
+void game_menu(){
+
+}
+
+void game_play(){
+
+}
+
+void game_over(){
+
+}
+
+void destroy_game(){
+
+}
--- dev/gameStates.h
+++ dev/gameStates.h
@@ -1,9 +1,21 @@
 #ifndef _GAMESTATES_
 #define _GAMESTATES_
 
+//Carrega bibliotecas do allegro
+//Carrega arquivos do jogo
+void load_game();
 
+//Carega menu interativo
+void game_menu();
 
+//Carrega o jogo
+void game_play();
 
+//Tela de game over
+void game_over();
+
+//Destroi estruturas do jogo
+void destroy_game();
 
 
 #endif
